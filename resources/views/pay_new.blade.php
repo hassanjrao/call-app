0b54e5d80ee1eb05475f430c757cd8c09750041e
Hassan Javaid <hassanjrao@gmail.com>
2024-08-01 00:51:38 +0400

    added new stripe inte

--- resources/views/pay_new.blade.php
+++ resources/views/pay_new.blade.php
@@ -214,17 +214,17 @@
                         <div class="row">
                             <div class="col-lg-12 mb-4">
                                 <label for="name" style="font-size: smaller">Full Name*</label>
-                                <input name="name" type="text" id="name" placeholder="Nom et prénom"
+                                <input name="name" type="text" id="name" placeholder=" Nom Complet"
                                     class="form-control" required>
                             </div>
                             <div class="col-lg-12 mb-4">
                                 <label for="email" style="font-size: smaller">Email*</label>
-                                <input name="email" type="email" id="email" placeholder="E-mail"
+                                <input name="email" type="email" id="email" placeholder="Email Address"
                                     class="form-control" required>
                             </div>
                             <div class="col-lg-12">
                                 <label for="phone" style="font-size: smaller">Téléphone*</label>
-                                <input name="phone" type="tel" id="phone" placeholder="Phone"
+                                <input name="phone" type="tel" id="phone" placeholder="Numéro de téléphone"
                                     class="form-control" required>
                             </div>
                         </div>
@@ -240,7 +240,7 @@
                     </div>
                     <button id="submitBtn">
                         <div class="spinner hidden" id="spinner"></div>
-                        <span id="button-text">Pay now</span>
+                        <span id="button-text">Pay & Subscribe</span>
                     </button>
                     <div id="payment-message" class="hidden"></div>
 
