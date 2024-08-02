10802a8a3d345be22ece7edd26782831d85e3afe
Hassan Javaid <hassanjrao@gmail.com>
2024-08-03 01:19:13 +0400

    name phone nullable

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -18,82 +18,90 @@ class StripePaymentController extends Controller
 
         Log::info('StripePaymentController@subscribe', ['request' => $request->all()]);
 
-
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user) {
-            $user = User::find($request->user_id);
-
-            $user->update([
-                'email' => $request->email,
-            ]);
-        }
-
-        $fullNameArr = explode(' ', $request->name);
-        $firstName = $fullNameArr[0];
-        $lastName = isset($fullNameArr[1]) ? $fullNameArr[1] : $firstName;
-
-        $password = \Str::random(10);
-
-        session(['user_password' => $password]);
-
-
-        $user->update([
-            'first_name' => $firstName,
-            'last_name' => $lastName,
-            'is_temp' => 0,
-            'password' => \Hash::make($password),
-        ]);
-
-
-        $plan = Plan::find($request->plan_id);
-
-        session(['user_id' => $user->id]);
-        session(['plan_id' => $plan->id]);
-
-
-
-        $UserIP = $request->ip();
-
-        $country = "France"; // Default to France if no location data is found
-        $position = Location::get($UserIP);
-
-        if (is_object($position) && isset($position->countryName)) {
-            $country = $position->countryName;
-        } else {
-            // Log error or handle the case where position is not as expected
-            Log::error("Failed to retrieve location data for IP: $UserIP");
-        }
-
-
-        Customer::updateOrCreate([
-            'user_id' => $user->id,
-        ], [
-            'user_id' => $user->id,
-            'country' => $country,
-            'number' => $request->phone,
-            'plan_id' => $request->plan_id,
-            'payment_method_id' =1,
-        ]);
-
-
-
-
-
         try {
 
-            $user->newSubscription('cashier', $plan->stripe_id)->create($request->paymentMethod, [
-                'email' => $user->email,
-                'name' => $user->first_name . ' ' . $user->last_name,
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user) {
+                $user = User::find($request->user_id);
+
+                $user->update([
+                    'email' => $request->email,
+                ]);
+            }
+
+            $fullNameArr = explode(' ', $request->name);
+            $firstName = $fullNameArr[0];
+            $lastName = isset($fullNameArr[1]) ? $fullNameArr[1] : $firstName;
+
+            $password = \Str::random(10);
+
+            session(['user_password' => $password]);
+
+
+            $user->update([
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'is_temp' => 0,
+                'password' => \Hash::make($password),
             ]);
-        } catch (IncompletePayment $exception) {
-            return redirect()->route(
-                'cashier.payment',
-                [$exception->payment->id]
-            );
+
+
+            $plan = Plan::find($request->plan_id);
+
+            session(['user_id' => $user->id]);
+            session(['plan_id' => $plan->id]);
+
+
+
+            $UserIP = $request->ip();
+
+            $country = "France"; // Default to France if no location data is found
+            $position = Location::get($UserIP);
+
+            if (is_object($position) && isset($position->countryName)) {
+                $country = $position->countryName;
+            } else {
+                // Log error or handle the case where position is not as expected
+                Log::error("Failed to retrieve location data for IP: $UserIP");
+            }
+
+
+            Customer::updateOrCreate([
+                'user_id' => $user->id,
+            ], [
+                'user_id' => $user->id,
+                'country' => $country,
+                'number' => $request->phone,
+                'plan_id' => $request->plan_id,
+                'payment_method_id' => 1,
+            ]);
+
+
+
+
+
+            try {
+
+                $user->newSubscription('cashier', $plan->stripe_id)->create($request->paymentMethod, [
+                    'email' => $user->email,
+                    'name' => $user->first_name . ' ' . $user->last_name,
+                ]);
+            } catch (IncompletePayment $exception) {
+                return redirect()->route(
+                    'cashier.payment',
+                    [$exception->payment->id]
+                );
+            }
+
+
+            return redirect()->route('thankYou', ['user_id' => $user->id, 'plan_id' => $plan->id]);
+        } catch (\Exception $e) {
+            Log::error('StripePaymentController@subscribe', [
+                'error' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+            return redirect()->back()->with('error', 'An error occurred. Please try again later.');
         }
-
-
-        return redirect()->route('thankYou', ['user_id' => $user->id, 'plan_id' => $plan->id]);
     }
 }
